feafd21b3be81824a64ced0e13f143e4ea9dcd0f
bogdanD <90522144+capraljohn@users.noreply.github.com>
2022-01-23 23:18:14 +0200

    Delete keys.dev.js

--- keys/keys.dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    SECRET_KEY: 'some secret value',
-    MONGODB_URI: 'mongodb+srv://bogdan:UB17PvuPSoBK3C9K@cluster0.tzvlv.mongodb.net/shop',
-    SENDGRID_API_KEY: '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',
-    EMAIL_FROM: 'dolzhanski4@gmail.com',
-    BASE_URL: 'http://localhost:3000'
-}
